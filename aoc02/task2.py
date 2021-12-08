e83ad8488d108a892a91af13e34efef9d0244f82
Robbe De Proft <rdeproft12@gmail.com>
2021-12-08 19:26:16 +0100

    Finish day 2

---
+++ aoc02/task2.py
@@ -0,0 +1,20 @@
+with open('input.txt') as input_file:
+    commands = []
+    for line in input_file:
+        command_arr = line.rstrip('\n').split(' ')
+        commands.append((command_arr[0], int(command_arr[1])))
+
+    aim = 0
+    depth = 0
+    horizontal = 0
+
+    for command in commands:
+        if command[0] == 'down':
+            aim += command[1]
+        elif command[0] == 'up':
+            aim -= command[1]
+        elif command[0] == 'forward':
+            horizontal += command[1]
+            depth += aim * command[1]
+
+    print(depth * horizontal)
