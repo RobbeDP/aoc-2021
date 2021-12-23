ca33f8810a341b1d3171d83e4a621405432ffa21
Robbe De Proft <rdeproft12@gmail.com>
2021-12-23 20:11:44 +0100

    Day 14 part 1

---
+++ aoc14/task1.py
@@ -0,0 +1,41 @@
+from collections import Counter
+
+
+def read_input(file_name):
+    with open(file_name) as input_file:
+        polymer_template = input_file.readline().rstrip('\n')
+        # Read empty line
+        input_file.readline()
+
+        pair_insertions = {}
+        for line in input_file:
+            pair, to_insert = line.rstrip('\n').split(' -> ')
+            pair_insertions[pair] = to_insert
+
+        return polymer_template, pair_insertions
+
+
+def do_insertions(polymer_template, pair_insertions, steps):
+    polymer_template = list(polymer_template)
+
+    for _ in range(steps):
+        for i in range(len(polymer_template) - 1):
+            index = 2 * i
+            pair = f'{polymer_template[index]}{polymer_template[index + 1]}'
+            to_insert = pair_insertions[pair]
+            polymer_template.insert(index + 1, to_insert)
+
+    return ''.join(polymer_template)
+
+
+def find_answer(file_name, steps):
+    polymer_template, pair_insertions = read_input(file_name)
+    polymer = do_insertions(polymer_template, pair_insertions, steps)
+
+    counts = Counter(polymer)
+
+    return max(counts.values()) - min(counts.values())
+
+
+if __name__ == '__main__':
+    print(find_answer('input.txt', 10))
